3cb60d3f151be31c913861b11588535005ee22dc
jyoti12singh <126713583+jyoti12singh@users.noreply.github.com>
2023-05-14 21:19:18 +0530

    Add files via upload

---
+++ Log_Toast_211b157/app/src/main/java/com/example/a211b157_lecture14/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.a211b157_lecture14;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void disp(View view)
+    {
+        Log.i("disp","there is an error here");
+        Toast.makeText( this,"Hello", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
